36be15d150c3f408a40d03a202cdbbc99c3895c3
Egor Menshikov <r3born.fs@gmail.com>
2026-03-28 01:30:22 +0300

    main: v1.0.0

---
+++ ch10learngoex.go
@@ -0,0 +1,6 @@
+// Package ch10learngoex is a chapter 10 exercise package from "Learning Go" 2nd ed by Jon Bodner
+package ch10learngoex
+
+func Add(a, b int) int {
+	return a + b
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/egor-menshikov/ch10learngoex
+
+go 1.26.1
