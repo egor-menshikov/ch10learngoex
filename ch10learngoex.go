7b0828f621ed8b35544ffc012bbfd50b87b5d36f
Egor Menshikov <r3born.fs@gmail.com>
2026-03-28 01:35:25 +0300

    main: godoc comments added

--- ch10learngoex.go
+++ ch10learngoex.go
@@ -1,6 +1,8 @@
 // Package ch10learngoex is a chapter 10 exercise package from "Learning Go" 2nd ed by Jon Bodner
 package ch10learngoex
 
+// Add adds two integers together and returns the result.
+// For more information on addition, see https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
